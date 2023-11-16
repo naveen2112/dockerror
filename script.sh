6d2b8d242c4b4e84aaf190558ac7f4deec621a10
naveen2112 <103127721+naveen2112@users.noreply.github.com>
2023-11-16 18:38:38 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-rake db:create
-rake db:migrate
-rake assets:precompile
-rails s
+bundle exec rake db:create
+bundle exec rake db:migrate
+bundle exec rake assets:precompile
+bundle exec rails s -b 0.0.0.0
+
